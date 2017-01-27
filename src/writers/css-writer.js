9c2fdd93e839220e3a431b349a78468a3cb40686
Gean Carlo Peixoto <geancpeixoto@gmail.com.br>
2017-01-27 18:20:06 -0300

    Corrigido renderização no ie10

--- src/writers/css-writer.js
+++ src/writers/css-writer.js
@@ -1,4 +1,3 @@
-const postcss = require('../utils/postcss');
 const fs = require('../utils/fs');
 
 class CSSWriter {
@@ -13,55 +12,49 @@ class CSSWriter {
       alias,
     } = this.fontface;
 
-    return postcss.process({
-      '@font-face': {
-        'font-family': name,
-        'font-style': 'normal',
-        'font-weight': 400,
-        src: [
-          `url(${alias}.eot)`,
-          `local(${name}),
-          local(${alias}),
-          url(${alias}.woff2) format('woff2'),
-          url(${alias}.woff) format('woff'),
-          url(${alias}.ttf) format('truetype')`,
-        ],
-      },
-      [`.${alias}`]: {
-        'font-family': name,
-        'font-weight': 'normal',
-        'font-style': 'normal',
-        display: 'inline-block',
-        'line-height': 1,
-        'text-transform': 'none',
-        'letter-spacing': 'normal',
-        'word-wrap': 'normal',
-        'white-space': 'nowrap',
-        direction: 'ltr',
-        '-webkit-font-smoothing': 'antialiased',
-        '-moz-osx-font-smoothing': 'grayscale',
-        'text-rendering': 'optimizeLegibility',
-        'font-feature-settings': 'liga',
-        overflow: 'hidden',
-      },
-      [`.${alias}.dp24`]: {
-        'font-size': '24px',
-        width: '24px',
-        height: '24px',
-      },
-      [`.${alias}.dp36`]: {
-        'font-size': '36px',
-        width: '36px',
-        height: '36px',
-      },
-      [`.${alias}.dp48`]: {
-        'font-size': '48px',
-        width: '48px',
-        height: '48px',
-      },
-    })
-    .then(data => fs.writeFile(filename, data)
-      .then(() => data));
+    const css = `
+@font-face {
+  font-family: '${name}';
+  font-style: normal;
+  font-weight: 400;
+  src: url('./${alias}.eot');
+  src: local('${name}'),
+    url('./${alias}.woff') format('woff'),
+    url('./${alias}.woff2') format('woff2'),
+    url('./${alias}.ttf') format('truetype');
+}
+
+.${alias} {
+  font-family: '${name}';
+  text-rendering: optimizeLegibility;
+  font-feature-settings: "liga" 1;
+  font-style: normal;
+  text-transform: none;
+  line-height: 1;
+  display: inline-block;
+  overflow: hidden;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.${alias}.dp24 {
+  font-size: 24px;
+  width: 24px;
+  height: 24px;
+}
+.${alias}.dp36 {
+  font-size: 36px;
+  width: 36px;
+  height: 36px;
+}
+.${alias}.dp48 {
+  font-size: 48px;
+  width: 48px;
+  height: 48px;
+}
+    `;
+
+    return fs.writeFile(filename, css)
+      .then(() => css);
   }
 }
 
--- src/writers/oldcss-writer.js
+++ src/writers/oldcss-writer.js
@@ -1,6 +1,4 @@
 /* eslint no-param-reassing: false */
-
-const postcss = require('../utils/postcss');
 const fs = require('../utils/fs');
 
 class OldCSSWriter {
@@ -16,66 +14,62 @@ class OldCSSWriter {
       glyphs,
     } = this.fontface;
 
-    const icons = glyphs.reduce((ret, glyph) => {
-      ret[`.${alias}-${glyph.name}:before`] = {
-        content: `'${glyph.unicode[0]}'`,
-      };
-      return ret;
-    }, {});
+    const icons = glyphs
+      .map(glyph => `.${alias}-${glyph.name}:before { content: '${glyph.unicode[0]}' }` )
+      .join('\n');
 
     function makeClass(append = '') {
       return `[class^="${alias}-"]${append}, [class*=" ${alias}-"]${append}`;
     }
 
-    return postcss.process(Object.assign({
-      '@font-face': {
-        'font-family': name,
-        'font-style': 'normal',
-        'font-weight': 400,
-        src: [
-          `url(${alias}.eot)`,
-          `local(${name}),
-          local(${alias}),
-          url(${alias}.woff2) format('woff2'),
-          url(${alias}.woff) format('woff'),
-          url(${alias}.ttf) format('truetype')`,
-        ],
-      },
-      [makeClass()]: {
-        'font-family': name,
-        'font-weight': 'normal',
-        'font-style': 'normal',
-        display: 'inline-block',
-        'line-height': 1,
-        'text-transform': 'none',
-        'letter-spacing': 'normal',
-        'word-wrap': 'normal',
-        'white-space': 'nowrap',
-        direction: 'ltr',
-        '-webkit-font-smoothing': 'antialiased',
-        '-moz-osx-font-smoothing': 'grayscale',
-        'text-rendering': 'optimizeLegibility',
-        'font-feature-settings': 'liga',
-        overflow: 'hidden',
-      },
-      [makeClass('.dp24')]: {
-        'font-size': '24px',
-        width: '24px',
-        height: '24px',
-      },
-      [makeClass('.dp36')]: {
-        'font-size': '36px',
-        width: '36px',
-        height: '36px',
-      },
-      [makeClass('.dp48')]: {
-        'font-size': '48px',
-        width: '48px',
-        height: '48px',
-      },
-    }, icons))
-      .then(data => fs.writeFile(filename, data)
-        .then(() => data));
+    const css = `
+@font-face {
+  font-family: '${name}';
+  font-style: normal;
+  font-weight: 400;
+  src: url('./${alias}.eot');
+  src: local('${name}'),
+    url('./${alias}.woff') format('woff'),
+    url('./${alias}.woff2') format('woff2'),
+    url('./${alias}.ttf') format('truetype');
+}
+
+${makeClass()} {
+  font-family: '${name}';
+  text-rendering: optimizeLegibility;
+  font-feature-settings: "liga" 1;
+  font-style: normal;
+  text-transform: none;
+  line-height: 1;
+  display: inline-block;
+  overflow: hidden;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+${makeClass('.dp24')} {
+  font-size: 24px;
+  width: 24px;
+  height: 24px;
+}
+
+${makeClass('.dp36')} {
+  font-size: 36px;
+  width: 36px;
+  height: 36px;
+}
+
+${makeClass('.dp48')} {
+  font-size: 48px,
+  width: 48px,
+  height: 48px,
+}
+
+${icons}
+`;
+
+    return fs.writeFile(filename, css)
+        .then(() => css);
   }
 }
 
